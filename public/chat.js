09aae82388ebc88afa7e48c046ab0dba96c0c8ce
Vasa <vasanth.krishnamoorthy@citrix.com>
2015-02-06 15:41:16 -0800

    Basic working example using jQuery

--- public/chat.js
+++ public/chat.js
@@ -1,42 +1,43 @@
-window.onload = function() {
+(function ($) {
 
     var messages = [],
         socket = io.connect('http://localhost:3700'),
-        field = document.getElementById("field"),
-        sendButton = document.getElementById("send"),
-        content = document.getElementById("content"),
-        name = document.getElementById("name");
+        field = $("#field"),
+        sendButton = $("#send"),
+        content = $("#content"),
+        name = $("#name"),
+        html;
 
     socket.on('message', function (data) {
-        if(data.message) {
+        if (data.message) {
             messages.push(data);
-            var html = '';
-            for(var i=0; i<messages.length; i++) {
+            html = '';
+            for (var i = 0; i < messages.length; i++) {
                 html += '<b>' + (messages[i].username ? messages[i].username : 'Server') + ': </b>';
                 html += messages[i].message + '<br />';
             }
-            content.innerHTML = html;
+            content.html(html);
         } else {
             console.log("There is a problem:", data);
         }
     });
 
-    sendButton.onclick = sendMessage = function() {
-        if(name.value == "") {
+    var sendMessage = function () {
+        if (name.val() == "") {
             alert("Please type your name!");
         } else {
-            var text = field.value;
-            socket.emit('send', { message: text, username: name.value });
+            var text = field.val();
+            socket.emit('send', {message: text, username: name.val()});
             field.value = "";
         }
     };
 
-}
+    sendButton.on('click', sendMessage);
 
-$(document).ready(function() {
-    $("#field").keyup(function(e) {
-        if(e.keyCode == 13) {
+    $("#field").keyup(function (e) {
+        if (e.keyCode == 13) {
             sendMessage();
         }
     });
-});
+
+})(jQuery);
